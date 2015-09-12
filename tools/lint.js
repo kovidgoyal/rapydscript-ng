d8a32fbcb6a2e3af153c3db85d5c0e43c373d9b2
Kovid Goyal <kovid@kovidgoyal.net>
2015-09-12 14:59:04 +0530

    Implement iteration over NamedNodeMap instances

--- tools/lint.js
+++ tools/lint.js
@@ -43,7 +43,7 @@ BUILTINS = {
     'AttributeError':true, 'KeyError':true, 'ValueError':true, 'map':true, 'hex':true,
     'filter':true, 'zip':true, 'equals': true, 'dict':true, 'dict_wrap':true,
     'HTMLCollection': true, 'NodeList': true, 'alert': true, 'console': true,
-    'Node':true, 'Symbol':true,
+    'Node':true, 'Symbol':true, 'NamedNodeMap':true,
 };
 Object.keys(RapydScript.NATIVE_CLASSES).forEach(function (name) { BUILTINS[name] = true; });
 
