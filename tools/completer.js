2514e2c40ce8b050c663825a426d21f9879f884c
Kovid Goyal <kovid@kovidgoyal.net>
2016-03-11 19:31:38 +0530

    Refactor the REPL completer into its own module

---
+++ tools/completer.js
@@ -0,0 +1,130 @@
+/* vim:fileencoding=utf-8
+ * 
+ * Copyright (C) 2016 Kovid Goyal <kovid at kovidgoyal.net>
+ *
+ * Distributed under terms of the GPLv3 license
+ */
+
+
+module.exports = function(compiler, options) {
+    "use strict";
+    var all_keywords = compiler.ALL_KEYWORDS.split(' ');
+    var vm = require('vm');
+    if (!options.enum_global) options.enum_global = "var global = Function('return this')(); Object.getOwnPropertyNames(global);";
+
+    function global_names(ctx) {
+        try {
+            var ans = vm.runInContext(options.enum_global, ctx);
+            ans = ans.concat(all_keywords);
+            ans.sort();
+            var seen = {};
+            ans.filter(function (item) { 
+                if (Object.prototype.hasOwnProperty.call(seen, item)) return false;
+                seen[item] = true;
+                return true;
+            });
+            return ans;
+        } catch(e) {
+            console.log(e.stack || e.toString());
+        }
+        return [];
+    }
+
+    function object_names(obj, prefix) {
+        if (obj === null || obj === undefined) return [];
+        var groups = [], prefix_len = prefix.length, p;
+
+        function prefix_filter(name) { return (prefix_len) ? (name.substr(0, prefix_len) === prefix) : true; }
+
+        function add(o) {
+            var items = Object.getOwnPropertyNames(o).filter(prefix_filter);
+            if (items.length) groups.push(items);
+        }
+
+        if (typeof obj === 'object' || typeof obj === 'function') {
+            add(obj);
+            p = Object.getPrototypeOf(obj);
+        } else p = obj.constructor ? obj.constructor.prototype : null; 
+
+        // Walk the prototype chain
+        try {
+            var sentinel = 5;
+            while (p !== null && sentinel > 0) {
+                add(p);
+                p = Object.getPrototypeOf(p);
+                // Circular refs possible? Let's guard against that.
+                sentinel--;
+            }
+        } catch (e) {
+            // console.error("completion error walking prototype chain:" + e);
+        }
+        if (!groups.length) return [];
+        var seen = {}, ans = [];
+        function uniq(name) {
+            if (Object.prototype.hasOwnProperty.call(seen, name)) return false;
+            seen[name] = true;
+            return true;
+        }
+        for (var i = 0; i < groups.length; i++) {
+            var group = groups[i];
+            group.sort();
+            ans = ans.concat(group.filter(uniq));
+            ans.push('');  // group separator
+
+        }
+        while (ans.length && ans[ans.length - 1] === '') ans.pop();
+        return ans;
+    }
+
+    function prefix_matches(prefix, items) {
+        var len = prefix.length;
+        var ans = items.filter(function(item) { return item.substr(0, len) === prefix; });
+        ans.sort();
+        return ans;
+    }
+
+    function find_completions(line, ctx) {
+        var t;
+        try {
+            t = compiler.tokenizer(line, '<repl>');
+        } catch(e) { return []; }
+        var tokens = [], token;
+        while (true) {
+            try {
+                token = t();
+            } catch (e) { return []; }
+            if (token.type === 'eof') break;
+            if (token.type === 'punc' && '(){},;:'.indexOf(token.value) > -1)
+                tokens = [];
+            tokens.push(token);
+        }
+        if (!tokens.length) {
+            // New line or trailing space
+            return [global_names(ctx), ''];
+        }
+        var last_tok = tokens[tokens.length - 1];
+        if (last_tok.value === '.' || (last_tok.type === 'name' && compiler.IDENTIFIER_PAT.test(last_tok.value))) {
+            last_tok = last_tok.value;
+            if (last_tok === '.') {
+                tokens.push({'value':''});
+                last_tok = '';
+            }
+            if (tokens.length > 1 && tokens[tokens.length - 2].value === '.') {
+                // A compound expression
+                var prefix = '', result;
+                tokens.slice(0, tokens.length - 2).forEach(function (tok) { prefix += tok.value; });
+                if (prefix) {
+                    try {
+                        result = vm.runInContext(prefix, ctx, {'displayErrors':false});
+                    } catch(e) { return []; }
+                    return [object_names(result, last_tok), last_tok];
+                }
+            } else {
+                return [prefix_matches(last_tok, global_names(ctx)), last_tok];
+            }
+        }
+        return [];
+    }
+
+    return find_completions;
+};
--- tools/repl.js
+++ tools/repl.js
@@ -11,6 +11,7 @@ var path = require('path');
 var vm = require('vm');
 var util = require('util');
 var utils = require('./utils');
+var completelib = require('./completer');
 var colored = utils.safe_colored;
 var RapydScript = (typeof create_rapydscript_compiler === 'function') ? create_rapydscript_compiler() : require('./compiler').create_compiler();
 
@@ -25,8 +26,6 @@ function create_ctx(baselib, show_js, console) {
 
 var homedir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 var cachedir = expanduser(process.env.XDG_CACHE_HOME || '~/.cache');
-var all_keywords = RapydScript.ALL_KEYWORDS.split(' ');
-var enum_global = "var global = Function('return this')(); Object.getOwnPropertyNames(global);";
 
 function expanduser(x) {
   if (!x) return x;
@@ -51,7 +50,6 @@ function repl_defaults(options) {
     if (options.terminal === undefined) options.terminal = options.output.isTTY;
     if (options.histfile === undefined) options.histfile = path.join(cachedir, 'rapydscript-repl.history');
     if (options.baselib === undefined) options.baselib = read_baselib(options.lib_path);
-    if (!options.enum_global) options.enum_global = enum_global;
         
     options.colored = (options.terminal) ? colored : (function (string) { return string; });
     options.historySize = options.history_size || 1000;
@@ -75,122 +73,6 @@ function write_history(options, history) {
     }
 }
 
-// Completion {{{
-
-function global_names(ctx, options) {
-    try {
-        var ans = vm.runInContext(options.enum_global, ctx);
-        ans = ans.concat(all_keywords);
-        ans.sort();
-        var seen = {};
-        ans.filter(function (item) { 
-            if (Object.prototype.hasOwnProperty.call(seen, item)) return false;
-            seen[item] = true;
-            return true;
-        });
-        return ans;
-    } catch(e) {
-        console.log(e.stack || e.toString());
-    }
-    return [];
-}
-
-function object_names(obj, prefix) {
-    if (obj === null || obj === undefined) return [];
-    var groups = [], prefix_len = prefix.length, p;
-
-    function prefix_filter(name) { return (prefix_len) ? (name.substr(0, prefix_len) === prefix) : true; }
-
-    function add(o) {
-        var items = Object.getOwnPropertyNames(o).filter(prefix_filter);
-        if (items.length) groups.push(items);
-    }
-
-    if (typeof obj === 'object' || typeof obj === 'function') {
-        add(obj);
-        p = Object.getPrototypeOf(obj);
-    } else p = obj.constructor ? obj.constructor.prototype : null; 
-
-    // Walk the prototype chain
-    try {
-        var sentinel = 5;
-        while (p !== null && sentinel > 0) {
-            add(p);
-            p = Object.getPrototypeOf(p);
-            // Circular refs possible? Let's guard against that.
-            sentinel--;
-        }
-    } catch (e) {
-        // console.error("completion error walking prototype chain:" + e);
-    }
-    if (!groups.length) return [];
-    var seen = {}, ans = [];
-    function uniq(name) {
-        if (Object.prototype.hasOwnProperty.call(seen, name)) return false;
-        seen[name] = true;
-        return true;
-    }
-    for (var i = 0; i < groups.length; i++) {
-        var group = groups[i];
-        group.sort();
-        ans = ans.concat(group.filter(uniq));
-        ans.push('');  // group separator
-
-    }
-    while (ans.length && ans[ans.length - 1] === '') ans.pop();
-    return ans;
-}
-
-function prefix_matches(prefix, items) {
-    var len = prefix.length;
-    var ans = items.filter(function(item) { return item.substr(0, len) === prefix; });
-    ans.sort();
-    return ans;
-}
-
-function find_completions(line, ctx, options) {
-    var t;
-    try {
-        t = RapydScript.tokenizer(line, '<repl>');
-    } catch(e) { return []; }
-    var tokens = [], token;
-    while (true) {
-        try {
-            token = t();
-        } catch (e) { return []; }
-        if (token.type === 'eof') break;
-        if (token.type === 'punc' && '(){},;:'.indexOf(token.value) > -1)
-            tokens = [];
-        tokens.push(token);
-    }
-    if (!tokens.length) {
-        // New line or trailing space
-        return [global_names(ctx, options), ''];
-    }
-    var last_tok = tokens[tokens.length - 1];
-    if (last_tok.value === '.' || (last_tok.type === 'name' && RapydScript.IDENTIFIER_PAT.test(last_tok.value))) {
-        last_tok = last_tok.value;
-        if (last_tok === '.') {
-            tokens.push({'value':''});
-            last_tok = '';
-        }
-        if (tokens.length > 1 && tokens[tokens.length - 2].value === '.') {
-            // A compound expression
-            var prefix = '', result;
-            tokens.slice(0, tokens.length - 2).forEach(function (tok) { prefix += tok.value; });
-            if (prefix) {
-                try {
-                    result = vm.runInContext(prefix, ctx, {'displayErrors':false});
-                } catch(e) { return []; }
-                return [object_names(result, last_tok), last_tok];
-            }
-        } else {
-            return [prefix_matches(last_tok, global_names(ctx, options)), last_tok];
-        }
-    }
-    return [];
-}
-// }}}
 
 module.exports = function(options) {
 	var output_options = {'omit_baselib':true, 'write_name':false, 'private_scope':false, 'beautify':true};
@@ -205,6 +87,7 @@ module.exports = function(options) {
     var LINE_CONTINUATION_CHARS = ':\\';
     var toplevel;
     var import_dirs = utils.get_import_dirs();
+    var find_completions = completelib(RapydScript, options);
 
     options.console.log(options.colored('Welcome to the RapydScript REPL! Press Ctrl+C then Ctrl+D to quit.', 'green', true));
     if (options.show_js)
@@ -216,7 +99,7 @@ module.exports = function(options) {
     function resetbuffer() { buffer = []; }
 
     function completer(line) {
-        return find_completions(line, ctx, options);
+        return find_completions(line, ctx);
     }
 
     function prompt() {
