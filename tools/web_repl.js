c26c710ba581b9edb4aba61c59afc09a0b9d8ca2
Kovid Goyal <kovid@kovidgoyal.net>
2016-03-11 13:35:02 +0530

    web-repl: Fix handling of classes and redirect print

--- tools/web_repl.js
+++ tools/web_repl.js
@@ -13,21 +13,41 @@ module.exports = function(compiler, baselib) {
     compiler.AST_Node.warn_function = function() {};
     var ctx = vm.createContext();
     vm.runInContext(baselib, ctx);
+    vm.runInContext('var __name__ = "__repl__";', ctx);
 
     return {
         'toplevel': null,
+
+        'replace_print': function replace_print(write_line_func) {
+            ctx.print = function() {
+                var parts = [];
+                for (var i = 0; i < arguments.length; i++) 
+                    parts.push(ctx._$rapyd$_str(arguments[i]));
+                write_line_func(parts.join(' '));
+            };
+        },
+
         'compile': function web_repl_compile(code, options) {
+            var classes = (this.toplevel) ? this.toplevel.classes : undefined;
             options = options || {};
             this.toplevel = compiler.parse(code, {
-                filename: '<repl>',
-                basedir: '::',
+                'filename': '<repl>',
+                'basedir': '::',
+                'classes': classes,
             });
             var out = compiler.OutputStream(output_options);
             this.toplevel.print(out);
             return out.toString();
         },
+
         'runjs': function runjs(code) {
-            vm.runInContext(code, ctx);
+            var ans = vm.runInContext(code, ctx);
+            if (ans !== undefined || ans === null) {
+                ctx._$rapyd$_repl_val = ans;
+                var q = vm.runInContext('_$rapyd$_repr(_$rapyd$_repl_val)', ctx);
+                ans = (q === 'undefined') ? ans.toString() : q;
+            }
+            return ans;
         },
     };
 };
--- web-repl/main.js
+++ web-repl/main.js
@@ -17,9 +17,20 @@
         return web_repl.runjs(code);
     }
 
-    function add_output(text) {
+    function println(text) {
+        document.getElementById('output').appendChild(document.createTextNode(text + '\n'));
+    }
+
+    function add_output(text, color) {
         var output = document.getElementById('output');
-        output.appendChild(document.createTextNode(text));
+        var node = document.createTextNode(text);
+        if (color) {
+            var e = document.createElement('span');
+            e.style.color = color;
+            e.appendChild(node);
+            node = e;
+        }
+        output.appendChild(node);
         output.appendChild(document.createElement('hr'));
     }
 
@@ -29,12 +40,49 @@
         output.appendChild(document.createElement('hr'));
     }
 
+    function read_eval(code) {
+        var js, obj, text;
+        try {
+            js = compile(code);
+        } catch(e) {
+            if (e.is_eof && e.line == buffer.length && e.col > 0) return true;
+            if (e.message && e.line !== undefined) text = ('Error:' + e.line + ':' + e.col + ':' + e.message);
+            else text = e.stack || e.toString();
+            add_output(text, 'red');
+            return false;
+        }
+        add_javascript(js);
+        try {
+            obj = runjs(js);
+        } catch(e) {
+            add_output(e.toString(), 'red');
+            return;
+        }
+        if (typeof obj === 'string') add_output(obj, 'brown');
+        return false;
+    }
+
+    function run_code() {
+        var input = document.getElementById('input');
+        var code = input.value;
+        input.value = '';
+        if (code) read_eval(code);
+        document.getElementById('input').focus();
+    }
+
     function on_load() {
         web_repl = exports.web_repl();
+        web_repl.replace_print(println);
         document.getElementById('loading').style.display = 'none';
         document.getElementById('top').style.display = 'flex';
         document.getElementById('bottom').style.display = 'flex';
-        add_javascript(compile('a = "a"; alert(a)'));
+        add_output(
+            ('RapydScript-ng ' + exports.rs_version + '\n' +
+            'Type RapydScript code into the box at the bottom and click the' + 
+            ' Run button.'), 'blue'
+        );
+        document.getElementById('run').addEventListener('click', run_code);
+        document.getElementById('input').focus();
     }
 
     window.onload = on_load;
