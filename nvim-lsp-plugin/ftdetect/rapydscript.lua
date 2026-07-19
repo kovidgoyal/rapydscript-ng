be6ce630821aeee56089f64232fd6a4ae9456413
Kovid Goyal <kovid@kovidgoyal.net>
2026-07-19 21:54:44 +0530

    ftdetect for nvim plugin

---
+++ nvim-lsp-plugin/ftdetect/rapydscript.lua
@@ -0,0 +1,10 @@
+#! /usr/bin/env lua
+--
+-- rapydscript.lua
+-- Copyright (C) 2026 Kovid Goyal <kovid at kovidgoyal.net>
+--
+-- Distributed under terms of the MIT license.
+--
+
+
+vim.filetype.add({ extension = { pyj = 'rapydscript' } })
